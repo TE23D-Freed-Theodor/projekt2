44ea4b1b4b44db95b2264510cf5e5a3ae72010a5
Theodor Freed <theodor.freed@elev.ga.ntig.se>
2026-05-11 10:35:51 +0200

    Nu har jag lagt till en meny i Main och importerat ArrayList och Scanner

--- src/main/java/com/example/libsys/Main.java
+++ src/main/java/com/example/libsys/Main.java
@@ -1,7 +1,28 @@
 package com.example.libsys;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Book> books = new ArrayList<>();
+        ArrayList<Magazine> magazines = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Här är min vackra meny >:)");
+            System.out.println("1. Hämta böcker");
+            System.out.println("2. Hämta tidningar");
+            System.out.println("3. Visa alla hämtade saker");
+            System.out.println("4. Addera bok");
+            System.out.println("5. Addera tidning");
+            System.out.println("6. Stäng programmet");
+
+            int val = scanner.nextInt();
+
+            System.out.println(val);
+        }
+
     }
 }
